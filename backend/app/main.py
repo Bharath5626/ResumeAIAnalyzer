9069eed54b7bbe3d9c00b6fbf20fffcb8acef2ee
Bharath5626 <Bharath5626520@gmail.com>
2026-07-13 13:07:27 +0530

    Configure production cors URL

--- backend/app/main.py
+++ backend/app/main.py
@@ -33,6 +33,8 @@ app.add_middleware(
         "http://localhost:5174",
         "http://127.0.0.1:5173",
         "http://127.0.0.1:5174",
+        "https://resumeaianalyzer-geqk.onrender.com",
+        "https://resumeaianalyzer-geqk.onrender.com/docs",
     ],
     allow_credentials=True,
     allow_methods=["*"],
